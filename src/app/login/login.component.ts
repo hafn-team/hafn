186cb78c30b6d974cba177dcd6afa3f42d28736b
med-alaa-rb <alaa.rabai@gmail.com>
2020-10-07 15:47:47 +0100

    working athontification

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,32 @@
+import { collectExternalReferences } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent implements OnInit {
+  constructor(private _http: HttpService , private router: Router) {}
+  objData: any = [];
+
+  ngOnInit(): void {}
+
+  collectlogin(username, password) {
+    console.log(username, password);
+    this.objData = {
+      username: username,
+      password: password,
+    };
+    this._http.logUser(this.objData).subscribe((data) => {
+      if(data){
+        this.router.navigateByUrl('/home');
+      }
+      else{
+        alert("somthing wrong")
+      }
+    });
+  }
+}
